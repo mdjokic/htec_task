fba88e5a0b7a1832e61875a7ea9a57abdbd8d5b8
mdjokic <djokic.mihailo@gmail.com>
2020-06-27 22:52:28 +0200

    initialized project structure

---
+++ src/test/java/htec/task/FlightAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package htec.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
